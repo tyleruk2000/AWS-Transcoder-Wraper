ee49307dd2b36876969e40d84fdd47342f3d01b9
Tyler Allen <tyler@the-tech-tutorial.com>
2016-05-24 00:17:26 +0100

    Added region selector

--- src/MioAwsTrancoder.java
+++ src/MioAwsTrancoder.java
@@ -18,7 +18,6 @@ public class MioAwsTrancoder {
 
     private AWSCredentials credentials;
 
-
     public MioAwsTrancoder(final String accessKey, final String secretKey) {
         credentials = new AWSCredentials() {
             @Override
@@ -33,9 +32,14 @@ public class MioAwsTrancoder {
         };
     }
 
-    public String createJob(String fileName, String outputFilename, String pipelineId){
+    //1080: 1351620000001-000001
+    //720: 1351620000001-000010
+    //480: 1351620000001-000020
+    //HLS: 1351620000001-200015
+    // Preset ID: "1351620000001-000020" This will generate a 480p 16:9 mp4 output.
+    public String createJob(String fileName, String outputFilename, String pipelineId, String regionString, String presetId){
         AmazonElasticTranscoder amazonElasticTranscoder = new AmazonElasticTranscoderClient(credentials);
-        amazonElasticTranscoder.setRegion(Region.getRegion(Regions.EU_WEST_1));
+        amazonElasticTranscoder.setRegion(Region.getRegion(getRegion(regionString)));
 
         // Setup the job input using the provided input key.
         JobInput input = new JobInput().withKey(fileName);
@@ -44,7 +48,7 @@ public class MioAwsTrancoder {
         List<CreateJobOutput> outputs = new ArrayList<CreateJobOutput>();
         CreateJobOutput output = new CreateJobOutput()
                 .withKey(outputFilename)
-                .withPresetId("1351620000001-000020"); // This will generate a 480p 16:9 mp4 output.
+                .withPresetId(presetId);
         outputs.add(output);
 
         // Create a job on the specified pipeline and return the job ID.
@@ -56,14 +60,27 @@ public class MioAwsTrancoder {
         return amazonElasticTranscoder.createJob(createJobRequest).getJob().getId();
     }
 
-    public String getJobStatus(String jobId){
+    public String getJobStatus(String jobId, String regionString){
 
         AmazonElasticTranscoder amazonElasticTranscoder = new AmazonElasticTranscoderClient(credentials);
-        amazonElasticTranscoder.setRegion(Region.getRegion(Regions.EU_WEST_1));
+        amazonElasticTranscoder.setRegion(Region.getRegion(getRegion(regionString)));
 
         ReadJobRequest jobRequest = new ReadJobRequest()
                 .withId(jobId);
 
         return amazonElasticTranscoder.readJob(jobRequest).getJob().getStatus();
+    }
+
+    private Regions getRegion(String regionString){
+        if (regionString.equals("EU_WEST_1")){
+            return Regions.EU_WEST_1;
+        }else if (regionString.equals("US_WEST_1")){
+            return Regions.US_WEST_1;
+        }if (regionString.equals("US_WEST_2")){
+            return Regions.US_WEST_2;
+        }if (regionString.equals("US_EAST_1")){
+            return Regions.US_EAST_1;
+        }
+        return Regions.EU_WEST_1;
     }
 }
